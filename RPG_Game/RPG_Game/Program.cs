093de3bce3f415a1af7a57a4c1ccf640c70abfa1
YaroslavStep <yaroslav20319@gmail.com>
2025-12-09 12:34:35 +0200

    Created class Item

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -9,6 +9,21 @@ namespace RPG_Game
         void Equip();
         void Unequip();
     }
+
+    public abstract class Item
+    {
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+
+        protected Item(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public abstract void Use(Player player);
+    }
+
     internal class Program
     {
         static void Main(string[] args)
